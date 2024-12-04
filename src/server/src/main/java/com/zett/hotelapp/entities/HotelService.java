386b9e6196166eee7e2532cd894745561a8dabcf
Nguyen Quang <quangnn906@gmail.com>
2024-12-04 17:54:18 +0700

    BE: Add base entity classes and user-related entities for hotel application

---
+++ src/server/src/main/java/com/zett/hotelapp/entities/HotelService.java
@@ -0,0 +1,16 @@
+package com.zett.hotelapp.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "hotel_services")
+public class HotelService extends MasterEntityBase {
+   
+    @Column(columnDefinition = "NVARCHAR(255)", nullable = false, unique = true)
+    private String name;
+
+    private double price;
+}
